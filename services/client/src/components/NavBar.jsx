c1d744db3a59558b8a051f0680ef7c341396c89c
Mahamat S <max@max.com>
2018-03-24 17:19:44 +0300

    Login and register forms

--- services/client/src/components/NavBar.jsx
+++ services/client/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-const NavBar = (props) => (
+const NavBar = props => (
   <Navbar inverse collapseOnSelect>
     <Navbar.Header>
       <Navbar.Brand>
@@ -18,23 +18,31 @@ const NavBar = (props) => (
         <LinkContainer to="/about">
           <NavItem eventKey={2}>About</NavItem>
         </LinkContainer>
-        <LinkContainer to="/status">
-          <NavItem eventKey={3}>User Status</NavItem>
-        </LinkContainer>
+        {props.isAuthenticated && (
+          <LinkContainer to="/status">
+            <NavItem eventKey={3}>User Status</NavItem>
+          </LinkContainer>
+        )}
       </Nav>
       <Nav pullRight>
-        <LinkContainer to="/register">
-          <NavItem eventKey={1}>Register</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/login">
-          <NavItem eventKey={2}>Log In</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/logout">
-          <NavItem eventKey={3}>Log Out</NavItem>
-        </LinkContainer>
+        {!props.isAuthenticated && (
+          <LinkContainer to="/register">
+            <NavItem eventKey={1}>Register</NavItem>
+          </LinkContainer>
+        )}
+        {!props.isAuthenticated && (
+          <LinkContainer to="/login">
+            <NavItem eventKey={2}>Log In</NavItem>
+          </LinkContainer>
+        )}
+        {props.isAuthenticated && (
+          <LinkContainer to="/logout">
+            <NavItem eventKey={3}>Log Out</NavItem>
+          </LinkContainer>
+        )}
       </Nav>
     </Navbar.Collapse>
   </Navbar>
-)
+);
 
 export default NavBar;
